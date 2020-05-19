3191a3d5cb2a3da5ff12c3f8c281788a903943ec
Alexander Jacks <alexanderthejacks@gmail.com>
2020-05-18 17:14:48 -0700

    :construction: local API call w/o CORS

--- src/App.js
+++ src/App.js
@@ -2,19 +2,28 @@ import React from 'react';
 import './App.css';
 
 function App() {
+  // React hooks syntax
+  const [bag, fillBag] = React.useState("click for API")
+  
+  // API test handler
+  const handler = () =>
+    fetch( "https://icanhazdadjoke.com/", {headers: {accept: "Accept: application/json"}} ) // required http
+      .then( x => x.json() ) // format response into JSON...
+      .then( ({bag}) => fillBag(bag) ) // local state from API call
+
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Off we go!
-        </p>
+        <h1>Stocktwits</h1>
+        <button onClick={handler}><h2>Click to test API</h2></button>
+        <p>API sez: {bag}</p>
         <a
           className="App-link"
           href="https://j4cks.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Jacks Consulting
+          <p>Jacks Consulting</p>
         </a>
       </header>
     </div>
